6edf56940e3a41578502dcb1a1705baceaabf9cc
Davidosky <Davidosky1@gmail.com>
2020-04-13 01:26:05 +0100

    Update index.js
    server update

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`));
